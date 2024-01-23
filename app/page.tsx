b1a20500916561d792f7d22d37fdf8141dc47bb2
arifire21 <ariwolfpup@icloud.com>
2024-01-23 13:51:10 -0500

    light/dark toggle

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 import styles from '@/styles/page.module.css'
-import GlimpseCard from '@/components/card'
+import {WorkCard, ProjectCard} from '@/components/card'
+import DarkModeSwitch from '@/components/switch'
 import EagleEyeLogo from '@/images/eagle-eye/logo_transparent_crop.png'
 import LEILogo from '@/images/loucks-eng/logo192.png'
 import SilviLogo from '@/images/silvi/silvi_logo.webp'
@@ -8,45 +9,94 @@ import JustLearnLogo from '@/images/justlearn/justlearn_logo.webp'
 export default function Home() {
   return (
     <>
-    <h1>Professional Experience</h1>
+    <DarkModeSwitch/>
     <main className={styles.main}>
-      <GlimpseCard
-        logo={EagleEyeLogo}
-        position='Website Designer and Developer'
-        company='Eagle Eye Monitoring, LLC.'
-        jobType='Freelance (Remote)'
-        duration='Nov 2023 - Present'
-        blurb='Figma | TypeScript | SCSS | Next.js | SendGrid | MaterialUI-Base'
-      />
+    <h1>Professional Experience</h1>
+      <div className={styles.cardContainer}>
+        <WorkCard
+          logo={EagleEyeLogo}
+          position='Website Designer and Developer'
+          company='Eagle Eye Monitoring, LLC.'
+          jobType='Freelance (Remote)'
+          duration='Nov 2023 - Present'
+          blurb='Figma | TypeScript | SCSS | Next.js | SendGrid | MaterialUI-Base'
+          link='/professional/eagle-eye'
+        />
 
-      <GlimpseCard
-        logo={LEILogo}
-        position='Website Developer'
-        company='Loucks Engineering, Inc.'
-        jobType='Part-Time'
-        duration='May 2023 - Present'
-        blurb='React.js | CD/CI (Heroku) | JavaScript | React-Bootstrap | JSON'
-      />
+        <WorkCard
+          logo={LEILogo}
+          position='Website Developer'
+          company='Loucks Engineering, Inc.'
+          jobType='Part-Time'
+          duration='May 2023 - Present'
+          blurb='React.js | JavaScript | React-Bootstrap | JSON | CD/CI (Heroku)'
+          link='/professional/loucks-eng'
+        />
 
-      <GlimpseCard
-        logo={SilviLogo}
-        position='Front-End Web Developer'
-        company='Silvi'
-        jobType='Part-Time (Remote)'
-        duration='Jun 2022 - Jul 2023'
-        blurb='TypeScript | SCSS | Next.js | APIs | in-browser camera'
-      />
+        <WorkCard
+          logo={SilviLogo}
+          position='Front-End Web Developer'
+          company='Silvi'
+          jobType='Part-Time (Remote)'
+          duration='Jun 2022 - Jul 2023'
+          blurb='TypeScript | SCSS | Next.js | APIs | in-browser camera'
+          link='/professional/silvi'
+        />
 
-      <GlimpseCard
-        logo={JustLearnLogo}
-        position='Front-End Web Developer'
-        company='JustLearn'
-        jobType='Internship & Part-Time (Remote)'
-        duration='Nov 2021 - June 2022'
-        blurb='Django | HTML5 | SCSS | JavaScript | Python | jQuery | Web APIs | Bootstrap'
-      />
-      </main>
+        <WorkCard
+          logo={JustLearnLogo}
+          position='Front-End Web Developer'
+          company='JustLearn'
+          jobType='Internship & Part-Time (Remote)'
+          duration='Nov 2021 - June 2022'
+          blurb='Django | HTML5 | SCSS | JavaScript | Python | jQuery | Web APIs | Bootstrap'
+          link='/professional/justlearn'
+        />
+      </div>
+
       <h1>Projects</h1>
+      <div className={styles.cardContainer}>
+        <ProjectCard
+          projectTitle='Discord.js Bot'
+          subtitle=''
+          date='Nov 2022 - Present'
+          blurb='Discord.js | Node.js | Axios | WikiMedia API'
+          link='projects/other'
+        />
+
+        <ProjectCard
+          projectTitle='Discord.js Bot'
+          subtitle=''
+          date='Nov 2022 - Present'
+          blurb='Discord.js | Node.js | Axios | WikiMedia API'
+          link='projects/other'
+        />
+
+        <ProjectCard
+          projectTitle='Plant Watering Notification System'
+          subtitle='Secure IoT Final Project'
+          date='Apr - May 2021'
+          blurb='Arduino Wifi 1000 | MailChimp'
+          link='projects/arduino'
+        />
+
+        <ProjectCard
+          projectTitle='Discord.js Bot'
+          subtitle=''
+          date='Nov 2022 - Present'
+          blurb='Discord.js | Node.js | Axios | WikiMedia API'
+          link='projects/other'
+        />
+
+        <ProjectCard
+          projectTitle='Dog-Walking Alarm Clock'
+          subtitle='Hands-On Engineering Design Final Project'
+          date='Aug - Dec 2020'
+          blurb='Arduino Uno R3 | Solidworks'
+          link='projects/arduino'
+        />
+      </div>
+      </main>
       </>
   )
 }
---
+++ components/switch.jsx
@@ -0,0 +1,32 @@
+"use client"
+import { Switch } from '@mui/joy'
+import { useColorScheme } from '@mui/joy/styles';
+import { useEffect, useState } from 'react';
+import { FaSun, FaMoon } from "react-icons/fa6";
+
+export default function DarkModeSwitch(){
+    const [dark, setDark] = useState(false)
+    const { mode, systemMode, setMode } = useColorScheme();
+
+    useEffect(() => {
+        //'mode' is defaulted to 'system' (before localstorage takes over)
+        //detect system pref on load
+        console.log(`${mode}, ${systemMode}`)
+        if(mode || systemMode === 'dark'){
+            console.log('dark is true')
+            setDark(true)
+        }
+    }, [])
+
+    return(
+        <Switch
+            aria-label='light/dark mode'
+            color={dark ? 'success' : 'neutral'}  //purple hex : gray hex
+            checked={dark}
+            onChange={(event) => setDark(event.target.checked)}
+            onClick={() => setMode(mode === 'dark' ? 'light' : 'dark')}
+            startDecorator={<FaSun color={dark ? 'gray' : 'green'}/>}
+            endDecorator={<FaMoon color={dark ? 'green' : 'gray'}/>}
+        />
+    )
+}
